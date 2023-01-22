27ee4f6cd3ff0b87236cfce8d46b41f2fb0b029b
akram6t <khanakram8435@gmail.com>
2023-01-22 19:32:21 +0530

    initial commit

---
+++ keywords.js
@@ -0,0 +1,35 @@
+const keywords = [
+    {
+        color: 'cyan',
+        words: [
+            'if', 'else', 'for', 'while',
+            'do', 'break', 'continue', 'switch', 'case', 
+            'default', 'try', 'catch', 'finally', 'throw','return'
+        ]
+    },
+    {
+        color: 'orange',
+        words: [
+            'var', "let", 'const', 'function', 'class', 'import', 'export'
+        ]
+    },
+    {
+        color: 'greenyellow',
+        words: [
+            'this', 'super', 'constructor', 'extends'
+        ]
+    },
+    {
+        color: 'yellow',
+        words: ['[', ']', ',', '+','-', '*','+=','-=', '^=', '*=', '/=', ' = ']
+    },
+    {
+        color: 'aqua',
+        words: ['1',"2",'3','4',"5","6",'7','8','9', '0']
+    },
+    {
+        color: 'red',
+        words: ['null']
+    },
+
+]
